73506be34114b46efc5ec286314b9f35ad50b6b3
imsamurai <im.samuray@gmail.com>
2013-07-12 14:46:52 +0300

    remove garbage

--- Model/Datasource/Http/TwitterSource.php
+++ Model/Datasource/Http/TwitterSource.php
@@ -10,43 +10,11 @@ App::uses('HttpSource', 'HttpSource.Model/Datasource');
 
 class TwitterSource extends HttpSource {
 
-    /**
-     * Twitter API Datasource
-     *
-     * @var string
-     */
-    public $description = 'TwitterSource DataSource';
+	/**
+	 * Twitter API Datasource
+	 *
+	 * @var string
+	 */
+	public $description = 'TwitterSource DataSource';
 
-
-//	 /**
-//     * Sets credentials data and fetch auth header
-//     *
-//     * @param array $credentials
-//     */
-//    public function setCredentials(array $credentials = array()) {
-//        if (empty($credentials['oauth_token'])) {
-//            throw new HttpSourceException('oauth_token not found in credentials');
-//        }
-//        if (empty($credentials['oauth_token_secret'])) {
-//            throw new HttpSourceException('oauth_token_secret not found in credentials');
-//        }
-//
-//        parent::setCredentials($credentials);
-//    }
-//
-//	/**
-//	 * {@inheritdoc}
-//	 */
-//    public function beforeRequest($request, $request_method) {
-//		$request['auth'] = array(
-//			'auth' => array(
-//				'method' => 'OAuth',
-//				'oauth_token' => $this->_credentials['token'],
-//				'oauth_token_secret' => $this->_credentials['secret'],
-//				'oauth_consumer_key' => Configure::read('Opauth.Strategy.Twitter.key'),
-//				'oauth_consumer_secret' => Configure::read('Opauth.Strategy.Twitter.secret')
-//		));
-////        $this->Http->configAuth('GoogleReaderSource.GoogleClientLogin', $this->credentials);
-//        return $request;
-//    }
 }
